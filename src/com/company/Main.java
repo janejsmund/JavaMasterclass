9aaee98a800f90207d49c8c1edfb7c84fd5f1184
Jan Ejsmund <jan.ejsmund@gmail.com>
2018-02-13 15:48:21 +0100

    linkedlist init

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,42 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Customer customer = new Customer("Tim", 54.96);
+        Customer anotherCustomer;
+        anotherCustomer = customer;
+        anotherCustomer.setBalance(12.18);
+        System.out.println("Balance for customer " + customer
+                .getName() + " is " + customer.getBalance());
 
+        ArrayList<Integer> intList = new ArrayList<>();
+
+        intList.add(1);
+        intList.add(3);
+        intList.add(4);
+
+        for (int i = 0;  i < intList.size(); i++) {
+            System.out.println(i + ": " + intList.get(i));
+        }
+
+        intList.add(1, 2);
+
+        for (int i = 0;  i < intList.size(); i++) {
+            System.out.println(i + ": " + intList.get(i));
+        }
+
+        LinkedList<Integer> integerLinkedList = new LinkedList<>();
+        integerLinkedList.add(1);
+        integerLinkedList.add(3);
+        integerLinkedList.add(4);
+
+        for (int i = 0;  i < integerLinkedList.size(); i++) {
+            System.out.println(i + ": " + integerLinkedList.get(i));
+        }
     }
 }
